49616d9fb8b36fb6c5d8997c02be354f064a7fa4
nklkarthi <nklkarthi@gmail.com>
2024-03-29 11:34:41 +0800

    Update the remote state

--- terraform/11-kubernetes.tf
+++ terraform/11-kubernetes.tf
@@ -10,21 +10,4 @@ data "terraform_remote_state" "eks" {
   }
 }
 
-data "aws_eks_cluster" "eks" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
-}
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      data.aws_eks_cluster.cluster.name
-    ]
-  }
-}
